360e4a102f298c5dc7e55053fd2116ba1c2c1731
Wyatt987 <52wbedor@frhsd.com>
2025-09-16 13:48:39 -0400

    finish pasting roaster code

---
+++ majors.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () =>  {
+
+    const grid= document.getElementById('rosterGrid')
+
+    const render = list => {
+        grid.innerHTML = ''
+        list.forEach(p => {
+            const col = document.createElement('div')
+            col.className = 'col-6 col-lg-4 py-2'
+
+            col.innerHTML = 
+    `
+    <div class="card h-100 shadow-sm position-relative">
+        <div class="card-blur-content"> <!-- This div is used to create a blur effect on the card when the popup is open -->
+            <img src="${p.photo}" alt="${p.firstName} ${p.lastName}" class="card-img-top">
+            <div class="card-body text-center">
+                <h5 class="card-title mv-1">${p.firstName} ${p.lastName}</h5>
+                <div class="badge badge-position text-dark badge-pos-${p.position}">${p.position}</div>
+                <p class="small text-muted mb-0">Age ${p.age}</p>
+                <button class="btn btn-primary mt-2" onclick="toggleBlur(this.closest('.card'))">More Details</button>
+            </div>
+        </div>
+        <div class="popup d-none p-2 d-flex justify-content-center align-items-center bg-dark bg-opacity-25 position-absolute top-0 start-0 w-100 h-100">
+            <p class="outlinedText fs-4">${p.detail}</p>
+            <p><button class="btn btn-primary btn-sm ms-2" onclick="closePopup(this.closest('.card'))">Close</button></p>
+        </div>
+    </div>
+    `
+
+            grid.appendChild(col)
+    })
+    }
+
+
+
+
+
+
+    render(players)
+} )
--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 const height = document.getElementById('height');
 const Left = document.getElementById('Left');
 const Right = document.getElementById('Right');
+const reveal = document.getElementById('reveal');
   function ScrollDown() {
     height.classList.remove('d-none');
     Left.classList.remove('d-none');
     Right.classList.remove('d-none');
     Left.classList.add('left');
     Right.classList.add('right');
+    reveal.classList.remove('d-none');
+    reveal.classList.add('up');
   }
 
